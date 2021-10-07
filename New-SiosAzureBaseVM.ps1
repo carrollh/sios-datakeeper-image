c14a527cecec10b8ce9685bfec7088e1ae6cffc9
carrollh <heath.aaron.carroll@gmail.com>
2021-10-07 15:19:36 -0400

    static WS2019 version

---
+++ New-SiosAzureBaseVM.ps1
@@ -0,0 +1,110 @@
+# New-SiosAzureBaseVM.ps1
+# PS> .\New-SiosAzureBaseVM.ps1 -Product DKCE -Version 8.8.1 -OSVersion WS2019 -LicenseType PAYG -Verbose
+
+[CmdletBinding()]
+Param(
+    [Parameter(Mandatory=$True, Position=0)]
+    [ValidateSet("SPSL","DKCE")]
+    [String] $Product = '',
+
+    [Parameter(Mandatory=$True, Position=1)]
+    [String] $Version = '',
+
+    [Parameter(Mandatory=$True, Position=2)]
+    [ValidateSet("WS2012R2","WS2016","WS2019","RHEL79")]
+    [String] $OSVersion = '',
+
+    [Parameter(Mandatory=$True, Position=3)]
+    [ValidateSet("BYOL","PAYG")]
+    [String] $LicenseType = '',
+
+    [Parameter(Mandatory=$False)]
+    [ValidateSet('master','test','develop')]
+    [String] $Branch = 'main',
+
+    [Parameter(Mandatory=$False)]
+    [Switch] $SAP = $False
+)
+
+function Get-ParametersFromURL() {
+    Param(
+        [Parameter(Mandatory=$False,Position=0)]
+        [string] $URL
+    )
+    return (Invoke-WebRequest -Uri $URL | ConvertFrom-Json).parameters
+}
+
+function Get-ParametersFromFile() {
+    Param(
+        [Parameter(Mandatory=$False,Position=0)]
+        [string] $Path
+    )
+    return (Get-Content $Path | Out-String | ConvertFrom-Json).parameters
+}
+
+if ( $OutDir -eq $Null -Or $OutDir -eq "" ) {
+    $OutDir = $PSScriptRoot
+}
+
+### Parameter validation - START
+$parametersAreValid = $True
+
+if ( $SAP ) {
+    if ( $LicenseType -eq 'BYOL' ) {
+        Write-Error "`nSAP AMIs only support PAYG license model."
+        $parametersAreValid = $false
+    }
+
+    if ( $OSVersion -eq 'WS2012R2' ) {
+        Write-Error "`nSAP AMIs are only supported on WS2016+."
+        $parametersAreValid = $false
+    }
+}
+if(-Not $parametersAreValid) {
+    return 1
+}
+### Parameter validation - END
+
+### MAIN ##############################################################################
+$templateURLBase = "https://raw.githubusercontent.com/carrollh/sios-datakeeper-image/$($Branch)/azuredeploy.json"
+
+if ( $SAP ) {
+    $tag = "$($Product)v$($Version.Replace('.',''))forSAPon$($OSVersion.Replace('WS',''))-$($LicenseType)"
+}
+else {
+    $tag = "$($Product)v$($Version.Replace('.',''))on$($OSVersion.Replace('WS',''))-$($LicenseType)"
+}
+Write-Verbose "Starting $tag Image creation process..."
+
+# lookup who the user is running this script
+$qaUser = & "whoami"
+$qaUser = $qaUser.ToUpper().Replace("STEELEYE\",'')
+
+# get parameters for template deployment
+$parameterFilePath = "$($templateURLBase)/azuredeploy.parameters.json"
+$resourcePrefix = "$($Product)v$($Version.Replace('.',''))-$($OSVersion)"
+$parameters = [System.Collections.ArrayList] (Get-ParametersFromURL -URL $parameterFilePath)
+$parameters.networkInterfaceName = "$($resourcePrefix)-NIC"
+$parameters.publicIpAddressName = "$($resourcePrefix)-IP"
+$parameters.virtualMachineName = "$($resourcePrefix)"
+$parameters.subscriptionId = (az account show | ConvertFrom-Json).id
+$parameters.adminPassword = "SIOS!5105?sios"
+$parameters.userData = "echo 'userdata goes here' > C:\Windows\Temp\userdata.out"
+
+# format for verbose output
+$parameters | Format-Table | Out-String -Stream | Write-Verbose
+
+# format for azure cli acceptance
+$params = ""
+($parameters | Get-Member -MemberType NoteProperty).Name | foreach {
+    $params += "$($_)=$($parameters.$_.value) "
+}    
+
+$templateURL += "$($templateURLBase)/azuredeploy.json"
+
+
+Write-Verbose $templateURL
+
+$output = (az deployment group create --resource-group AzurePublishing --template-uri "$templateURL" --parameters $params | ConvertFrom-Json)
+
+return $output
